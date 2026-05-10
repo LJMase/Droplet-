cbd124a804757396db1a672d4a3d731eec6e172a
LJMase <ljmase40@gmail.com>
2026-05-10 16:26:50 -0500

    display_paint_menu() and paint.py completed. project.py updated and experimenting with json to r/w project data structure. Added new options to display_main_menu(), still wip.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
+import json
 from mini import Mini 
 from project import Project 
 from paint import Paint 
 
-mini_csv, paint_csv = "./miniatures.csv", "./paints.csv"
+mini_csv, paint_csv, project_csv = "./miniatures.csv", "./paints.csv", "./projects.csv"
 miniatures = []
 paints = []
+projects = []
 
 def load_objects():
     if os.path.isfile(mini_csv):
@@ -27,36 +29,89 @@ def load_objects():
         print("No paint file found. Creating new file...")
         with open(paint_csv, "w"):
             pass 
+    
+    if os.path.isfile(project_csv):  
+        with open(project_csv, "r") as file:
+            for line in file:
+                words = [word.strip() for word in line.split(",")]
+                projects.append(Project(words[0], words[1], words[2], words[3]))
+    else:
+        print("No project file found. Creating new file...")
+        with open(project_csv, "w"):
+            pass 
 
 def display_main_menu():
     while True:
         try:
-            choice = int(input("\nWelcome to Droplet! What would you like to do?\n1. Open a Project\n2. Change Miniatures\n3. Change Paints\n4. Exit\n"))
+            choice = int(input("\nWelcome to Droplet! What would you like to do?\n1. Open a Project\n2. Create a Project\n3. View All Projects\n4. Change Miniatures\n5. Change Paints\n6. Exit\n"))
         except ValueError:
-            print("\nWARNING: Must be a number 1-4.\n")
+            print("\nWARNING: Must be a number 1-6.\n")
             continue
             
         match choice:
             case 1:
-                pass
+                print("Please enter the project name and category: ")
+                project_name, project_category = input("Name: "), input("Category: ")
+                for project in projects:
+                    if project == Project(project_name, project_category):
+                        display_projects_menu(project)
+                        return 0
+                    if project == projects[len(projects)-1]:
+                        print("Project not found.")    
             case 2:
+                print("Enter the project category, name, and description.")
+                project_category, project_name, project_description = input("Category: "), input("Name: "), input("Description: ")
+                projects.append(Project(project_category, project_name, project_description))
+            case 3:
+                for i in range(0, len(projects)):
+                    print(f"{i+1}. {sorted(projects)[i]}")
+            case 4:
                 display_miniatures_menu()
                 return 0
-            case 3:
+            case 5:
                 display_paints_menu()
                 return 0
-            case 4:
+            case 6:
                 with open(mini_csv, "w") as file:
                     for mini in miniatures:
-                        file.write(f"{mini.game}, {mini.faction}, {mini.name}\n")
+                        json.dump(f"{mini.game}, {mini.faction}, {mini.name}", file)
 
                 with open(paint_csv, "w") as file:
                     for paint in paints:
-                        file.write(f"{paint.brand}, {paint.color}, {paint.sku}\n")
+                        json.dump(f"{paint.brand}, {paint.color}, {paint.sku}", file)
+
+                with open(project_csv, "w") as file:
+                    for project in projects:
+                        json.dump(f"{project.category}, {project.name}, {project.description}, {project.minis}", file)
 
                 return 0 
             case _:
-                print("\nWARNING: Must be a number 1-4.\n")
+                print("\nWARNING: Must be a number 1-6.")
+
+def display_projects_menu(project):
+    while True:
+        try:
+            choice = int(input("\n1. Add a Mini\n2. View Minis\n3. Paint a Mini\n4. Change Mini Status\n5. Change Project Status\n6. Exit\n"))
+        except ValueError:
+            print("\nWARNING: Must be a number 1-6.\n")
+            continue
+
+        match choice:
+            case 1:
+                project.add_mini(miniatures[1])
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass 
+            case 5:
+                pass
+            case 6:
+                display_main_menu()
+                return 0
+            case _:
+                print("\nWARNING: Must be a number 1-6.\n") 
 
 def display_miniatures_menu():
     while True:
@@ -112,9 +167,27 @@ def display_paints_menu():
                 print("\nPlease enter the name, the brand, and the SKU (if N/A, enter 0).")
                 paints.append(Paint(input("Brand: "), input("Color: "), input("SKU: "))) 
             case 2:
-                pass
+                print("\nEnter the details of the paint you would like to edit: ")
+                brand, color, sku = input("Brand: "), input("Color: "), input("SKU (0 for none): ")
+                for paint in paints: 
+                    if paint == Paint(brand, color, sku):
+                        print("\nPaint Found! Enter the new details below:")
+                        new_brand, new_color, new_sku = input("New Brand: "), input("New Color: "), input("New SKU: ")
+                        paint.brand, paint.color, paint.sku = new_brand, new_color, new_sku
+                        break 
+                    # Using else here would print the not found result every for loop and putting it outside the for loop results in it running every time. This is my solution. 
+                    if paint == paints[len(paints)-1]:
+                        print("Paint Not Found")
             case 3:
-                pass
+                print("\nEnter the details of the paint you would like to delete:")
+                brand, color, sku = input("Brand: "), input("Color: "), input("SKU (0 for none): ")
+                for paint in paints:
+                    if paint == Paint(brand, color, sku):
+                        print("\nPaint Deleted!")
+                        paints.remove(paint)
+                        break
+                    if paint == paints[len(paints)-1]:
+                        print("Paint Not Found")
             case 4:
                 for i in range(0, len(paints)):
                     print(f"{i+1}: {sorted(paints)[i]}")
--- project.py
+++ project.py
@@ -1,18 +1,21 @@
 class Project():
-    def __init__(self, category, name, description):
-        self.__category = category 
-        self.__name = name 
-        self.__description = description
-        self.minis = []
+    def __init__(self, category, name, description="", minis=[]):
+        self.category = category 
+        self.name = name 
+        self.description = description
+        self.minis = {}
 
-    def add_mini(mini):
-        self.minis.append(mini)
-    
-    def get_category():
-        return self.__category
-    
-    def get_name():
-        return self.__name
-    
-    def get_description():
-        return self.__description
+    def add_mini(self, mini):
+        self.minis[str(mini)] = {"Amount": 0, "Paints": ""}
+        self.minis[str(mini)]["Status"] = {"On-Sprue": 0, "Assembled": 0, "Primed": 0, "Painted": 0}
+
+    def __eq__(self, other):
+        return self.name == other.name and self.category == other.category
+
+    def __str__(self):
+        return f"({self.category}) {self.name}: {self.description}"
+
+    def __lt__(self, other):
+        if self.name == other.name:
+            return self.category < other.category
+        return self.name < other.name    
